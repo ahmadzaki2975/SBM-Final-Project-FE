ccff9977843b7272555f0826a670271ded274000
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-09 20:13:59 +0700

    fix: sliding white bar effect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,8 +9,8 @@ export default function Home() {
     <main className="flex">
       <aside className={`bg-gradient-to-b from-[#2A58D0] via-[#5740B4] to-[#4291B4] w-[270px] fixed h-screen ${inter.className}`}>
         <h1 className="text-[#f2f2f2] font-semibold text-[36px] mt-20 text-center underline underline-4 underline-white">Project SBM</h1>
-        <ul className="text-[36px] flex flex-col //gap-[87px] items-center //bg-white fill-transparent">
-          <div className="w-full bg-[#f5f5f5] absolute h-[20%]"></div>
+        <ul className="relative text-[36px] flex flex-col //gap-[87px] items-center //bg-white fill-transparent">
+          <div className="w-full bg-[#f5f5f5] absolute h-[25%] //translate-y-[100%]"></div>
           <li className="flex items-center gap-8 z-[2] py-5 !bg-gradient-to-b from-[#2A58D0] via-[#5740B4] to-[#4291B4] !bg-clip-text text-transparent">
             Home
           </li>
