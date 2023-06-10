2d5e858fdcef06c717737736bff23c69d9c33818
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-10 07:18:11 +0700

    feat: add required pages

---
+++ src/pages/settings/index.jsx
@@ -0,0 +1,7 @@
+export default function SettingsPage() {
+  return (
+    <main className="flex min-h-screen justify-center items-center">
+      <h1 className="text-center text-black">Settings</h1>
+    </main>
+  );
+}
